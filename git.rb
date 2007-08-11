bd6ad3dd8f46502bccdd10d281750da7c11cda35
Wincent Colaiuta <win@wincent.com>
2007-08-12 01:51:32 +0200

    Initial version of Git build number scripts
    This commit adds a simple Git utility module which doesn't do much yet, just
    reports "revision numbers" (corresponding to the abbreviated hash of the
    most recent commit), and a script intended for use in an Xcode Shell Script
    build phase that leverages this module and uses it to create or update a
    "build number" file that can then be used for as a header for Info.plist
    preprocessing or other purposes.
    
    This replaces the old scripts which did the same thing in Subversion terms; in
    the end I decided to go with the abbreviated hash as the build number despite
    the fact that it's not very human intelligible: its most important attribute
    is that it uniquely identifies the exact version of the source code that was
    used to produce a build.
    
    Note that due to a bug in Git the "local changes" variant does not yet work
    (this is where the build number has a "+" appended to it to indicate local
    changes since the last commit).
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

---
+++ git.rb
@@ -0,0 +1,43 @@
+#!/usr/bin/env ruby
+# git.rb
+# buildtools
+#
+# Created by Wincent Colaiuta on 12 August 2007.
+# 
+# Copyright 2007 Wincent Colaiuta.
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+module Git
+  def self.get_rev_number
+    rev = command('git show -s --pretty=format:"%h" HEAD')
+    raise "'#{rev}' is not a valid revision number" unless rev =~ /\A[a-f0-9]{7}\z/
+
+    # append "+" if there are local modifications
+    `git diff --quiet HEAD`
+    rev = "#{rev}+" if $? != 0
+    rev
+  end
+
+private
+
+  # Runs the command passed in via string.
+  # Returns the output from the command with any trailing newline stripped.
+  # Raises an exception if the command has a non-zero exit status.
+  def self.command(string)
+    `#{string}`.chomp
+  ensure
+    raise 'non-zero exit status (#{$?.exitstatus}) for command: #{string}' if $?.exitstatus != 0
+  end
+
+end # module Git
