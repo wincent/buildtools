30cf542f4840a205bfd75343236d38f5e9829a47
Wincent Colaiuta <win@wincent.com>
2007-12-23 19:19:46 +0100

    Enhancements to UpdateXibs script
    The script now performs multiple functions: creates/updates strings
    files in the base language if required; creates xibs in the target
    languages if required; creates/updates strings files in the target
    languages if required; merges new strings from the base language
    strings files into the target language strings files; extracts
    incremental strings files (containing new and untranslated strings
    in the target language); and combines incremental strings files
    with existing translations.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- UpdateXibs.sh
+++ UpdateXibs.sh
@@ -22,6 +22,56 @@
 . "${BUILDTOOLS_DIR}/Common.sh"
 
 #
+# Functions
+#
+
+checking()
+{
+  echo "$BOLD[Checking]$RESET $1"
+}
+
+generating()
+{
+  echo "$BOLD[Generating]$RESET $1"
+}
+
+merging()
+{
+  echo "$BOLD[Merging]$RESET $1"
+}
+
+extracting()
+{
+  echo "$BOLD[Extracting]$RESET $1"
+}
+
+updating()
+{
+  echo "$BOLD[Updating]$RESET $1"
+}
+
+cloning()
+{
+  echo "$BOLD[Cloning]$RESET $1"
+}
+
+skipping()
+{
+  echo "$BOLD[Skipping]$RESET $1"
+}
+
+combining()
+{
+  echo "$BOLD[Combining]$RESET $1"
+}
+
+# returns 0 (success) if the two input files differ, 1 otherwise
+compare()
+{
+  ! diff "$1" "$2" > /dev/null 2>&1
+}
+
+#
 # Main
 #
 
@@ -34,20 +84,108 @@ cd $1
 BASE="en.lproj"
 test -d "$BASE" || die "base directory $BASE not found in $1"
 
-for LOC in $(find . -type d -name '*.lproj' -not -name "$BASE"); do
+# write output files to a temporary directory first so we can compare them for changes
+TMPDIR=$(mktemp -d /private/tmp/com.wincent.buildtools.UpdateXibs.XXXXXX) ||
+  die "couldn't create temporary directory"
+
+# create/update strings files in base language if required
+for BASE_XIB in $(find "$BASE" -name '*.xib'); do
+  BASE_STRINGS=$(echo "$BASE_XIB" | sed -e 's/\.xib/.strings/')
+  if [ ! -f "$BASE_STRINGS" ]; then
+    generating "Base strings file '$BASE_STRINGS'."
+    ibtool --generate-stringsfile "$BASE_STRINGS" "$BASE_XIB"
+  elif [ "$BASE_XIB" -nt "$BASE_STRINGS" ]; then
+    mkdir -p $(dirname "$TMPDIR/$BASE_STRINGS")
+    ibtool --generate-stringsfile "$TMPDIR/$BASE_STRINGS" "$BASE_XIB"
+    if compare "$BASE_STRINGS" "$TMPDIR/$BASE_STRINGS"; then
+      updating "Base strings file '$BASE_STRINGS'."
+      cp "$TMPDIR/$BASE_STRINGS" "$BASE_STRINGS"
+    else
+      skipping "Base strings file '$BASE_STRINGS' (already up-to-date)."
+    fi
+  fi
+done
+
+for LOC in $(find . -type d -name '*.lproj' -depth 1 -not -name "$BASE"); do
   for BASE_XIB in $(find "$BASE" -name '*.xib'); do
+
+    # create xibs in target language if required
     XIB=$(basename "$BASE_XIB")
     TARGET_XIB="$LOC/$XIB"
     if [ ! -f "$TARGET_XIB" ]; then
-      note "Target xib '$TARGET_XIB' does not exist; cloning from $BASE_XIB"
+      cloning "Target xib '$TARGET_XIB' from '$BASE_XIB'."
+      note "'$TARGET_XIB' created (must add to Xcode project)."
       cp "$BASE_XIB" "$TARGET_XIB"
     fi
 
-    TARGET_STRINGS=$(echo "$TARGET_XIB" | sed -e 's/.xib/.strings/')
+    # create/update strings files in target language if required
+    TARGET_STRINGS=$(echo "$TARGET_XIB" | sed -e 's/\.xib/.strings/')
     if [ ! -f "$TARGET_STRINGS" ]; then
-      echo "Target strings file '$TARGET_STRINGS' does not exist; generating."
+      generating "Target strings file '$TARGET_STRINGS'."
       ibtool --generate-stringsfile "$TARGET_STRINGS" "$TARGET_XIB"
+    elif [ "$TARGET_XIB" -nt "$TARGET_STRINGS" ]; then
+      mkdir -p $(dirname "$TMPDIR/$TARGET_STRINGS")
+      ibtool --generate-stringsfile "$TMPDIR/$TARGET_STRINGS" "$TARGET_XIB"
+      if compare "$TARGET_STRINGS" "$TMPDIR/$TARGET_STRINGS"; then
+        updating "Target strings file '$TARGET_STRINGS'."
+        cp "$TMPDIR/$TARGET_STRINGS" "$TARGET_STRINGS"
+      else
+        skipping "Target strings file '$TARGET_STRINGS' (already up-to-date)."
+      fi
     fi
+
+    # merge new strings from base langauge into target language strings files
+    BASE_STRINGS=$(echo "$BASE_XIB" | sed -e 's/\.xib/.strings/')
+    mkdir -p $(dirname "$TMPDIR/$TARGET_STRINGS")
+    wincent-strings-util -base "$BASE_STRINGS" \
+      -merge "$TARGET_STRINGS" \
+      -output "$TMPDIR/$TARGET_STRINGS" 2> /dev/null
+    if compare "$TARGET_STRINGS" "$TMPDIR/$TARGET_STRINGS"; then
+      updating "Target language strings file '$TARGET_STRINGS' (merging from base)."
+      wincent-strings-util -base "$BASE_STRINGS" \
+        -merge "$TARGET_STRINGS" \
+        -output "$TARGET_STRINGS"
+    else
+      skipping "Target language strings file '$TARGET_STRINGS' (already up-to-date)."
+    fi
+
+    # produce/update incremental strings file
+    INCREMENTAL=$(echo "$TARGET_STRINGS" | sed -e 's/\.strings/.untranslated.strings/')
+    if [ ! -f "$INCREMENTAL" ]; then
+      generating "Incremental strings file '$INCREMENTAL'."
+      wincent-strings-util -base "$BASE_STRINGS" \
+        -extract "$TARGET_STRINGS" \
+        -output "$INCREMENTAL"
+    elif [ "$TARGET_STRINGS" -nt "$INCREMENTAL" ]; then
+      mkdir -p $(dirname "$TMPDIR/$INCREMENTAL")
+      wincent-strings-util -base "$BASE_STRINGS" \
+        -extract "$TARGET_STRINGS" \
+        -output "$TMPDIR/$INCREMENTAL" 2> /dev/null
+      if compare "$TARGET_STRINGS" "$TMPDIR/$INCREMENTAL"; then
+        updating "Incremental strings file '$INCREMENTAL'."
+        wincent-strings-util -base "$BASE_STRINGS" \
+          -extract "$TARGET_STRINGS" \
+          -output "$INCREMENTAL"
+      else
+        skipping "Incremental strings file '$INCREMENTAL' (already up-to-date)."
+      fi
+    fi
+
+    # combine incremental strings files with existing files
+    wincent-strings-util -base "$TARGET_STRINGS" \
+      -combine "$INCREMENTAL" \
+      -output "$TMPDIR/$TARGET_STRINGS" 2> /dev/null
+    if compare "$TARGET_STRINGS" "$TMPDIR/$TARGET_STRINGS"; then
+      combining "'$INCREMENTAL' and '$TARGET_STRINGS'."
+      wincent-strings-util -base "$TARGET_STRINGS" \
+        -combine "$INCREMENTAL" \
+        -output "$TARGET_STRINGS"
+    else
+      skipping "Combination of '$INCREMENTAL' and '$TARGET_STRINGS' (already up-to-date)."
+    fi
+
+    # if old nib files exist try integrating latest strings
+
   done
 done
 
