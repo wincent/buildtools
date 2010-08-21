c5d8f5771948ab8af29933c1b3606a07508b9954
Wincent Colaiuta <win@wincent.com>
2010-08-21 21:42:53 +0200

    Reformat code comments for better display on narrow terminals
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- help/apple_help.rb
+++ help/apple_help.rb
@@ -39,53 +39,62 @@ module Walrus
         ENV['WALRUS_STYLE'] == 'web'
       end
 
-      # Returns true if the current page is the front page.
-      # Does this by checking to see if he value for 'tag' has been set to 'front_page'
+      # Returns true if the current page is the front page. Does this by
+      # checking to see if he value for 'tag' has been set to 'front_page'
       def front_page?
         get_value(:tag) == 'front_page'
       end
 
-      # Takes the abstract previously registered using the #set directive and generates meta markup for recognition by the Help Indexer.
-      # If no abstract has been explicitly registered tries falling back and using the page_title.
-      # If no page_title has been registered returns an empty string.
-      # In practice this method is never used by inheriting templates; it is used by the top-level templates from which the others inherit.
+      # Takes the abstract previously registered using the #set directive and
+      # generates meta markup for recognition by the Help Indexer. If no
+      # abstract has been explicitly registered tries falling back and using
+      # the page_title. If no page_title has been registered returns an empty
+      # string. In practice this method is never used by inheriting templates;
+      # it is used by the top-level templates from which the others inherit.
       def abstract
         abstract = get_value(:abstract)
         abstract = get_value(:page_title) if abstract == ''
         (abstract != '') ? %Q{<meta name="description" content="#{html_escape abstract}">} : ''
       end
 
-      # Allows you to define additional keywords that will be detected by the Help Indexer.
-      # Call this from the setup block in each template to ensure keywords are registered before the HTML head is emitted.
-      # There is no need to define keywords if those words already appear elsewhere on the page.
-      # This method is for adding keywords which do not exist in the page text (synonyms or common misspellings).
-      # As the list is comma separated and quote-delimited, your input values should not themselves include commas or quotes.
-      # Example: keywords('registering', 'registration')
+      # Allows you to define additional keywords that will be detected by the
+      # Help Indexer. Call this from the setup block in each template to ensure
+      # keywords are registered before the HTML head is emitted. There is no
+      # need to define keywords if those words already appear elsewhere on the
+      # page. This method is for adding keywords which do not exist in the page
+      # text (synonyms or common misspellings). As the list is comma separated
+      # and quote-delimited, your input values should not themselves include
+      # commas or quotes. Example: keywords('registering', 'registration')
       def keywords(*words)
         @keyword_list = words.join(', ') and '' # make sure no output gets echoed
       end
 
-      # Takes keywords previously registered using the keywords method and generates meta markup for recognition by the Help Indexer.
-      # If no keywords have been registered returns an empty string.
-      # In practice this method is never used by inheriting templates; it is used by the top-level templates from which the others inherit.
+      # Takes keywords previously registered using the keywords method and
+      # generates meta markup for recognition by the Help Indexer. If no
+      # keywords have been registered returns an empty string. In practice this
+      # method is never used by inheriting templates; it is used by the
+      # top-level templates from which the others inherit.
       def keyword_meta
         (@keyword_list.nil?) ? '' : %Q{<meta name="keywords" content="#{html_escape @keyword_list}">}
       end
 
       # Insert a link to the target page.
+      #
       # If text is nil the target text is used with underscores removed
       #
-      # In the future will be able to output two possible formats:
+      # This method can output in two possible formats:
+      #
       # - a Help Viewer anchor
       # - relative HTML link (for uploading to website)
-      # For now only outputs the former.
-      # Ultimately, link_to_category will output an empty string for the online format.
-      # Empty strings would be shown in the "see also" listing.
-      # link_to_category should therefore only be used in conjunction with the "see also" feature.
       #
-      # target should be an anchor or base filename.
-      # For example, to link to a page "foo_bar.html" target would be "foo_bar".
-      # text is the human-readable (clickable) text for the link.
+      # Ultimately, link_to_category will output an empty string for the online
+      # format. Empty strings would be shown in the "see also" listing.
+      # link_to_category should therefore only be used in conjunction with the
+      # "see also" feature.
+      #
+      # target should be an anchor or base filename. For example, to link to a
+      # page "foo_bar.html" target would be "foo_bar". text is the
+      # human-readable (clickable) text for the link.
       def link_to(target, text = nil)
         text = target.gsub(/_/, ' ') if text.nil?
         if web_based?
@@ -104,36 +113,45 @@ module Walrus
       end
 
       # Add the current page to the specified category or categories.
-      # Call this from the setup block in each template to ensure categories are registered before the HTML body starts emission.
-      # The category identifier, cat, need not be "human readable" and is not localized.
-      # Apple help books use lowercase, alphanumeric category names with no spaces, so it is recommended to follow the same convention.
+      #
+      # Call this from the setup block in each template to ensure categories
+      # are registered before the HTML body starts emission. The category
+      # identifier, cat, need not be "human readable" and is not localized.
+      # Apple help books use lowercase, alphanumeric category names with no
+      # spaces, so it is recommended to follow the same convention.
       def category(*cats)
         @categories = [] if @categories.nil?
         @categories.concat(cats) and '' # make sure no output gets echoed
       end
 
-      # Takes the previously registered categories and returns output that will be recognized by the Help Indexer.
-      # If no categories have been registered returns an empty string.
-      # In practice this method is never used by inheriting templates; it is used by the top-level templates from which the others inherit.
-      # Including an empty anchor tag is sufficient to include a page in a category.
-      # See: http://andymatuschak.org/articles/2005/12/18/help-with-apple-help
+      # Takes the previously registered categories and returns output that will
+      # be recognized by the Help Indexer.
+      #
+      # If no categories have been registered returns an empty string. In
+      # practice this method is never used by inheriting templates; it is used
+      # by the top-level templates from which the others inherit. Including an
+      # empty anchor tag is sufficient to include a page in a category. See:
+      # http://andymatuschak.org/articles/2005/12/18/help-with-apple-help
       def categories
         (@categories.nil?) ? '' : @categories.collect { |cat| %Q{<a name="#{html_escape cat}"></a>} }.join("\n")
       end
 
       # category is the name of the anchor in the HTML source (not localized).
-      # label is displayed in the header at the top of the generated page and also in the link; it should be localized.
-      # label is HTML escaped automatically.
-      # Apple help books use lowercase, alphanumeric category names with no spaces.
-      # Note that category links won't work on the web-based version of the help; no output is produced.
+      # label is displayed in the header at the top of the generated page and
+      # also in the link; it should be localized. label is HTML escaped
+      # automatically. Apple help books use lowercase, alphanumeric category
+      # names with no spaces. Note that category links won't work on the
+      # web-based version of the help; no output is produced.
       def link_to_category(category, label = nil)
         return '' if web_based?
 
         label = category.gsub(/_/, ' ') if label.nil?
         # note that the bookID is not quoted even though it contains a space
         # likewise, Other may contain spaces even though it is not quoted
-        # in Apple help books non-ASCII characters in Other are not escaped (note that the template is UTF-8 encoded)
-        # template and stylesheet are paths relative to the help book folder
+        #
+        # in Apple help books non-ASCII characters in Other are not escaped
+        # (note that the template is UTF-8 encoded) template and stylesheet are
+        # paths relative to the help book folder
         %Q{<a href="help:topic_list=#{html_escape category}
                          bookID=#{book_id}
                          template=autogen/generated_list.html
@@ -142,17 +160,21 @@ module Walrus
       end
 
       # Expects a link created using the link or link_to_category method.
-      # Appends the supplied link to the list of "See also" items for the current page.
+      # Appends the supplied link to the list of "See also" items for the
+      # current page.
       def see_also(link)
         @see_also = [] if @see_also.nil?
         (@see_also << link) and '' # make sure no output gets echoed
       end
 
-      # Takes the previously registered "See also" items and returns an HTML list representation.
-      # If no items have been registered returns an empty string.
-      # Note that this method should only be called from basic.tmpl or one of its subclasses
-      # (ie from a page that implements the see_also_div method)
-      # The in-template method is used for localization purposes, so that localization can take place independently of this class.
+      # Takes the previously registered "See also" items and returns an HTML
+      # list representation.
+      #
+      # If no items have been registered returns an empty string. Note that
+      # this method should only be called from basic.tmpl or one of its
+      # subclasses (ie from a page that implements the see_also_div method).
+      # The in-template method is used for localization purposes, so that
+      # localization can take place independently of this class.
       def see_also_list
         if @see_also.nil?
           ''
@@ -164,7 +186,8 @@ module Walrus
               ''  # handle blank category links in web-based version
             end
           end
-          # beware that all items could be ''; in that case shouldn't emit anything for web-based version
+          # beware that all items could be ''; in that case shouldn't emit
+          # anything for web-based version
           (list.any? { |item| item != '' }) ? lookup_and_return_placeholder(:see_also_div, list.join("\n")) : ''
         end
       end
@@ -175,7 +198,8 @@ module Walrus
         ''
       end
 
-      # TODO: could be expressed slightly more elegantly with a #for construct inside the template itself
+      # TODO: could be expressed slightly more elegantly with a #for construct
+      # inside the template itself
       def index_rows
         return '' if @index_items.nil?
         items = @index_items.collect do |item|
@@ -195,23 +219,27 @@ module Walrus
         WalrusCloth.new(text).to_html
       end
 
-      # Returns the value of the book_id placeholder, automatically HTML escaped.
+      # Returns the value of the book_id placeholder, automatically HTML
+      # escaped.
       def book_id
         html_escape(get_value(:book_id))
       end
 
-      # Returns the value of the book_icon placeholder, automatically URL encoded and HTML escaped.
+      # Returns the value of the book_icon placeholder, automatically URL
+      # encoded and HTML escaped.
       def book_icon
         html_escape(url_encode(get_value(:book_icon)))
       end
 
-      # Returns the value of the page_title placeholder, automatically HTML escaped.
+      # Returns the value of the page_title placeholder, automatically HTML
+      # escaped.
       def page_title
         html_escape(get_value(:page_title))
       end
 
-      # For this method to produce output the #set directive must have been previously used to tag a page
-      # with a particular label. Typically this should occur in the setup block.
+      # For this method to produce output the #set directive must have been
+      # previously used to tag a page with a particular label. Typically this
+      # should occur in the setup block.
       def tag
         tag = get_value(:tag)
         (tag != '') ? %Q{<a name="#{html_escape tag}"></a>\n} : ''
