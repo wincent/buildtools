1f129e7716e8f1d0e355c659a2a8087ea3af6ce9
Greg Hurrell <greg@hurrell.net>
2018-10-15 21:20:37 +0200

    Tweak to work with newer Ruby

--- UpdateGitRevisionNumbers.rb
+++ UpdateGitRevisionNumbers.rb
@@ -25,7 +25,7 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
-require File.join(File.dirname(__FILE__), 'git').to_s
+require_relative './git'
 require 'pathname'
 
 target  = Pathname.new 'com.wincent.buildtools.gitrev.h'
